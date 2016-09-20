b0765f61b5a60139377d664a074f3a460600e9cf
Greg Mercer <gmercer@stanford.edu>
2016-09-20 14:16:24 -0700

    Create dbsettings.php

---
+++ dbsettings.php
@@ -0,0 +1,15 @@
+$databases = array (
+'default' =>
+array (
+  'default' =>
+  array (
+    'database' => 'gsbmygsb0',
+    'username' => 'root',
+    'password' => 'root',
+    'host' => '127.0.0.1',
+    'port' => '33066',
+    'driver' => 'mysql',
+    'prefix' => '',
+  ),
+),
+);
